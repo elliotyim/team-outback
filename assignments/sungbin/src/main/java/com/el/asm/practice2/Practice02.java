5888a469d4ffdc0933452e7af5936dc041fc02b1
yimsb01 <yimsb02@naver.com>
2019-09-18 22:41:03 +0900

    practice #2 추가

--- assignments/sungbin/src/main/java/com/el/asm/practice2/Practice02.java
+++ assignments/sungbin/src/main/java/com/el/asm/practice2/Practice02.java
@@ -14,6 +14,7 @@ public class Practice02 {
     ApplicationContext appCtx =
         new AnnotationConfigApplicationContext("com.el.asm.practice2.config");
     
+    // FindAll
     List<Member> members = appCtx.getBean(MemberDao.class).findAll();
     for (Member m : members) {
       System.out.println(m);
