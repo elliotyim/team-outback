d00350536538707fb1808bf1f32dcef71e3c0530
huijeong365 <sarahcho87@gmail.com>
2019-09-25 08:37:19 +0900

    Revert "Revert "no message""
    This reverts commit 28c69245f4bac47055aa128639f0087e87ff8745.

--- assignments/huijeong/src/main/java/com/test/chj/practice01/RequestMappingHandlerMapping.java
+++ assignments/huijeong/src/main/java/com/test/chj/practice01/RequestMappingHandlerMapping.java
@@ -1,5 +1,27 @@
 package com.test.chj.practice01;
 
+import java.lang.reflect.Method;
+import java.util.HashMap;
+
 public class RequestMappingHandlerMapping {
 
+  HashMap<String, RequestHandler> handlerMap = new HashMap<>();
+
+  public void addRequestHandler(String name, Object bean, Method method) {
+    handlerMap.put(name, new RequestHandler(method, bean));
+  }
+  
+  public RequestHandler getRequestHandler(String name) {
+    return handlerMap.get(name);
+  }
+  
+  public static class RequestHandler {
+    public Method method;
+    public Object bean;
+    
+    public RequestHandler(Method method, Object bean) {
+      this.method = method;
+      this.bean = bean;
+    }
+  }
 }
